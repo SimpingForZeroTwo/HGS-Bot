6110d1268a3b5b294df66afad3d79dff79558068
SimpingForZeroTwo <akv489@gmail.com>
2021-05-19 16:08:07 +0530

    Alpha 0.1 (19/5/2021 4:00PM)

--- index.js
+++ index.js
@@ -158,30 +158,30 @@ client.on('message', message => {
         const playersEmbed = new Discord.MessageEmbed()
         .setColor('#ff5555')
         .addFields(
-            { name: players[0][1]==="Dead"?"~~`"+players[0][0]+"`~~":players[0][0], value: players[0][1]+"\n"+players[0][2]+"\nKills: "+players[0][3], inline: true },
-            { name: players[1][1]==="Dead"?"~~`"+players[1][0]+"`~~":players[1][0], value: players[1][1]+"\n"+players[1][2]+"\nKills: "+players[1][3], inline: true },
-            { name: players[2][1]==="Dead"?"~~`"+players[2][0]+"`~~":players[2][0], value: players[2][1]+"\n"+players[2][2]+"\nKills: "+players[2][3], inline: true },
-            { name: players[3][1]==="Dead"?"~~`"+players[3][0]+"`~~":players[3][0], value: players[3][1]+"\n"+players[3][2]+"\nKills: "+players[3][3], inline: true },
-            { name: players[4][1]==="Dead"?"~~`"+players[4][0]+"`~~":players[4][0], value: players[4][1]+"\n"+players[4][2]+"\nKills: "+players[4][3], inline: true },
-            { name: players[5][1]==="Dead"?"~~`"+players[5][0]+"`~~":players[5][0], value: players[5][1]+"\n"+players[5][2]+"\nKills: "+players[5][3], inline: true },
-            { name: players[6][1]==="Dead"?"~~`"+players[6][0]+"`~~":players[6][0], value: players[6][1]+"\n"+players[6][2]+"\nKills: "+players[6][3], inline: true },
-            { name: players[7][1]==="Dead"?"~~`"+players[7][0]+"`~~":players[7][0], value: players[7][1]+"\n"+players[7][2]+"\nKills: "+players[7][3], inline: true },
-            { name: players[8][1]==="Dead"?"~~`"+players[8][0]+"`~~":players[8][0], value: players[8][1]+"\n"+players[8][2]+"\nKills: "+players[8][3], inline: true },
-            { name: players[9][1]==="Dead"?"~~`"+players[9][0]+"`~~":players[9][0], value: players[9][1]+"\n"+players[9][2]+"\nKills: "+players[9][3], inline: true },
-            { name: players[10][1]==="Dead"?"~~`"+players[10][0]+"`~~":players[10][0], value: players[10][1]+"\n"+players[10][2]+"\nKills: "+players[10][3], inline: true },
-            { name: players[11][1]==="Dead"?"~~`"+players[11][0]+"`~~":players[11][0], value: players[11][1]+"\n"+players[11][2]+"\nKills: "+players[11][3], inline: true },
-            { name: players[12][1]==="Dead"?"~~`"+players[12][0]+"`~~":players[12][0], value: players[12][1]+"\n"+players[12][2]+"\nKills: "+players[12][3], inline: true },
-            { name: players[13][1]==="Dead"?"~~`"+players[13][0]+"`~~":players[13][0], value: players[13][1]+"\n"+players[13][2]+"\nKills: "+players[13][3], inline: true },
-            { name: players[14][1]==="Dead"?"~~`"+players[14][0]+"`~~":players[14][0], value: players[14][1]+"\n"+players[14][2]+"\nKills: "+players[14][3], inline: true },
-            { name: players[15][1]==="Dead"?"~~`"+players[15][0]+"`~~":players[15][0], value: players[15][1]+"\n"+players[15][2]+"\nKills: "+players[15][3], inline: true },
-            { name: players[16][1]==="Dead"?"~~`"+players[16][0]+"`~~":players[16][0], value: players[16][1]+"\n"+players[16][2]+"\nKills: "+players[16][3], inline: true },
-            { name: players[17][1]==="Dead"?"~~`"+players[17][0]+"`~~":players[17][0], value: players[17][1]+"\n"+players[17][2]+"\nKills: "+players[17][3], inline: true },
-            { name: players[18][1]==="Dead"?"~~`"+players[18][0]+"`~~":players[18][0], value: players[18][1]+"\n"+players[18][2]+"\nKills: "+players[18][3], inline: true },
-            { name: players[19][1]==="Dead"?"~~`"+players[19][0]+"`~~":players[19][0], value: players[19][1]+"\n"+players[19][2]+"\nKills: "+players[19][3], inline: true },
-            { name: players[20][1]==="Dead"?"~~`"+players[20][0]+"`~~":players[20][0], value: players[20][1]+"\n"+players[20][2]+"\nKills: "+players[20][3], inline: true },
-            { name: players[21][1]==="Dead"?"~~`"+players[21][0]+"`~~":players[21][0], value: players[21][1]+"\n"+players[21][2]+"\nKills: "+players[21][3], inline: true },
-            { name: players[22][1]==="Dead"?"~~`"+players[22][0]+"`~~":players[22][0], value: players[22][1]+"\n"+players[22][2]+"\nKills: "+players[22][3], inline: true },
-            { name: players[23][1]==="Dead"?"~~`"+players[23][0]+"`~~":players[23][0], value: players[23][1]+"\n"+players[23][2]+"\nKills: "+players[23][3], inline: true },
+            { name: players[0][1]==="Dead"?"~~`"+players[0][0]+"`~~":players[0][0], value: "```diff\n"+(players[0][1]==="Dead"?"-"+players[0][1]:"+"+players[0][1])+"\n```\n"+players[0][2]+"\nKills: "+players[0][3], inline: true },
+            { name: players[1][1]==="Dead"?"~~`"+players[1][0]+"`~~":players[1][0], value: "```diff\n"+(players[1][1]==="Dead"?"-"+players[1][1]:"+"+players[1][1])+"\n```\n"+players[1][2]+"\nKills: "+players[1][3], inline: true },
+            { name: players[2][1]==="Dead"?"~~`"+players[2][0]+"`~~":players[2][0], value: "```diff\n"+(players[2][1]==="Dead"?"-"+players[2][1]:"+"+players[2][1])+"\n```\n"+players[2][2]+"\nKills: "+players[2][3], inline: true },
+            { name: players[3][1]==="Dead"?"~~`"+players[3][0]+"`~~":players[3][0], value: "```diff\n"+(players[3][1]==="Dead"?"-"+players[3][1]:"+"+players[3][1])+"\n```\n"+players[3][2]+"\nKills: "+players[3][3], inline: true },
+            { name: players[4][1]==="Dead"?"~~`"+players[4][0]+"`~~":players[4][0], value: "```diff\n"+(players[4][1]==="Dead"?"-"+players[4][1]:"+"+players[4][1])+"\n```\n"+players[4][2]+"\nKills: "+players[4][3], inline: true },
+            { name: players[5][1]==="Dead"?"~~`"+players[5][0]+"`~~":players[5][0], value: "```diff\n"+(players[5][1]==="Dead"?"-"+players[5][1]:"+"+players[5][1])+"\n```\n"+players[5][2]+"\nKills: "+players[5][3], inline: true },
+            { name: players[6][1]==="Dead"?"~~`"+players[6][0]+"`~~":players[6][0], value: "```diff\n"+(players[6][1]==="Dead"?"-"+players[6][1]:"+"+players[6][1])+"\n```\n"+players[6][2]+"\nKills: "+players[6][3], inline: true },
+            { name: players[7][1]==="Dead"?"~~`"+players[7][0]+"`~~":players[7][0], value: "```diff\n"+(players[7][1]==="Dead"?"-"+players[7][1]:"+"+players[7][1])+"\n```\n"+players[7][2]+"\nKills: "+players[7][3], inline: true },
+            { name: players[8][1]==="Dead"?"~~`"+players[8][0]+"`~~":players[8][0], value: "```diff\n"+(players[8][1]==="Dead"?"-"+players[8][1]:"+"+players[8][1])+"\n```\n"+players[8][2]+"\nKills: "+players[8][3], inline: true },
+            { name: players[9][1]==="Dead"?"~~`"+players[9][0]+"`~~":players[9][0], value: "```diff\n"+(players[9][1]==="Dead"?"-"+players[9][1]:"+"+players[9][1])+"\n```\n"+players[9][2]+"\nKills: "+players[9][3], inline: true },
+            { name: players[10][1]==="Dead"?"~~`"+players[10][0]+"`~~":players[10][0], value: "```diff\n"+(players[10][1]==="Dead"?"-"+players[10][1]:"+"+players[10][1])+"\n```\n"+players[10][2]+"\nKills: "+players[10][3], inline: true },
+            { name: players[11][1]==="Dead"?"~~`"+players[11][0]+"`~~":players[11][0], value: "```diff\n"+(players[11][1]==="Dead"?"-"+players[11][1]:"+"+players[11][1])+"\n```\n"+players[11][2]+"\nKills: "+players[11][3], inline: true },
+            { name: players[12][1]==="Dead"?"~~`"+players[12][0]+"`~~":players[12][0], value: "```diff\n"+(players[12][1]==="Dead"?"-"+players[12][1]:"+"+players[12][1])+"\n```\n"+players[12][2]+"\nKills: "+players[12][3], inline: true },
+            { name: players[13][1]==="Dead"?"~~`"+players[13][0]+"`~~":players[13][0], value: "```diff\n"+(players[13][1]==="Dead"?"-"+players[13][1]:"+"+players[13][1])+"\n```\n"+players[13][2]+"\nKills: "+players[13][3], inline: true },
+            { name: players[14][1]==="Dead"?"~~`"+players[14][0]+"`~~":players[14][0], value: "```diff\n"+(players[14][1]==="Dead"?"-"+players[14][1]:"+"+players[14][1])+"\n```\n"+players[14][2]+"\nKills: "+players[14][3], inline: true },
+            { name: players[15][1]==="Dead"?"~~`"+players[15][0]+"`~~":players[15][0], value: "```diff\n"+(players[15][1]==="Dead"?"-"+players[15][1]:"+"+players[15][1])+"\n```\n"+players[15][2]+"\nKills: "+players[15][3], inline: true },
+            { name: players[16][1]==="Dead"?"~~`"+players[16][0]+"`~~":players[16][0], value: "```diff\n"+(players[16][1]==="Dead"?"-"+players[16][1]:"+"+players[16][1])+"\n```\n"+players[16][2]+"\nKills: "+players[16][3], inline: true },
+            { name: players[17][1]==="Dead"?"~~`"+players[17][0]+"`~~":players[17][0], value: "```diff\n"+(players[17][1]==="Dead"?"-"+players[17][1]:"+"+players[17][1])+"\n```\n"+players[17][2]+"\nKills: "+players[17][3], inline: true },
+            { name: players[18][1]==="Dead"?"~~`"+players[18][0]+"`~~":players[18][0], value: "```diff\n"+(players[18][1]==="Dead"?"-"+players[18][1]:"+"+players[18][1])+"\n```\n"+players[18][2]+"\nKills: "+players[18][3], inline: true },
+            { name: players[19][1]==="Dead"?"~~`"+players[19][0]+"`~~":players[19][0], value: "```diff\n"+(players[19][1]==="Dead"?"-"+players[19][1]:"+"+players[19][1])+"\n```\n"+players[19][2]+"\nKills: "+players[19][3], inline: true },
+            { name: players[20][1]==="Dead"?"~~`"+players[20][0]+"`~~":players[20][0], value: "```diff\n"+(players[20][1]==="Dead"?"-"+players[20][1]:"+"+players[20][1])+"\n```\n"+players[20][2]+"\nKills: "+players[20][3], inline: true },
+            { name: players[21][1]==="Dead"?"~~`"+players[21][0]+"`~~":players[21][0], value: "```diff\n"+(players[21][1]==="Dead"?"-"+players[21][1]:"+"+players[21][1])+"\n```\n"+players[21][2]+"\nKills: "+players[21][3], inline: true },
+            { name: players[22][1]==="Dead"?"~~`"+players[22][0]+"`~~":players[22][0], value: "```diff\n"+(players[22][1]==="Dead"?"-"+players[22][1]:"+"+players[22][1])+"\n```\n"+players[22][2]+"\nKills: "+players[22][3], inline: true },
+            { name: players[23][1]==="Dead"?"~~`"+players[23][0]+"`~~":players[23][0], value: "```diff\n"+(players[23][1]==="Dead"?"-"+players[23][1]:"+"+players[23][1])+"\n```\n"+players[23][2]+"\nKills: "+players[23][3], inline: true },
         )
         .setTimestamp()
         .setFooter('May the strongest tribute survive');
@@ -279,15 +279,24 @@ client.on('message', message => {
                 else if(j==2)
                 {
                     var k = Math.random()
-                    if(k<0.3 && f==0)
+                    if(k<0.2 && f==0)
                     message.channel.send("`"+players[arr[i]][0]+"` and `"+players[arr[i+1]][0]+"`"+d_lines[Math.floor(Math.random()*3)])
-                    else if(k<0.4)
-                    message.channel.send("`"+players[arr[i]][0]+"` attacks `"+players[arr[i+1]][0]+"`, but `"+players[arr[i+1]][0]+"` manages to escape")
-                    else if(k<0.5 && f==0)
-                    message.channel.send("`"+players[arr[i]][0]+"` defeats `"+players[arr[i+1]][0]+"` in a fight, but spares their life.")
+                    else if(k<0.3)
+                    message.channel.send("`"+players[arr[i]][0]+"` ambushes `"+players[arr[i+1]][0]+"`, but `"+players[arr[i+1]][0]+"` manages to escape")
+                    else if(k<0.4 && f==0)
+                    message.channel.send("`"+players[arr[i]][0]+"` defeats `"+players[arr[i+1]][0]+"` in a fight, but spares their life")
+                    else if(k<0.5)
+                    message.channel.send("`"+players[arr[i]][0]+"` secretly stalks `"+players[arr[i+1]][0]+"`")
+                    else if(k<0.55)
+                    {
+                        message.channel.send("`"+players[arr[i]][0]+"` smashes `"+players[arr[i+1]][0]+"'s` head with a bottle of vodka, *cyka blyat* style")
+                        players[arr[i]][3]++
+                        players[arr[i+1]][1]="Dead"
+                        arr.splice(i+1,1)
+                        i--
+                        console.log(arr)
+                    }
                     else if(k<0.6)
-                    message.channel.send("`"+players[arr[i]][0]+"` stalks `"+players[arr[i+1]][0]+"`")
-                    else if(k<0.65)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` stabs `"+players[arr[i+1]][0]+"` while his back is turned")
                         players[arr[i]][3]++
@@ -296,7 +305,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.7)
+                    else if(k<0.65)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` kills `"+players[arr[i+1]][0]+"` while he is resting")
                         players[arr[i]][3]++
@@ -305,7 +314,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.75)
+                    else if(k<0.7)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` strangles `"+players[arr[i+1]][0]+"` with a rope")
                         players[arr[i]][3]++
@@ -314,7 +323,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.8)
+                    else if(k<0.75)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` catches `"+players[arr[i+1]][0]+"` off guard and kills him")
                         players[arr[i]][3]++
@@ -323,7 +332,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.85)
+                    else if(k<0.8)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` tracks down and kills `"+players[arr[i+1]][0]+"`")
                         players[arr[i]][3]++
@@ -332,7 +341,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.9)
+                    else if(k<0.85)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` sets `"+players[arr[i+1]][0]+"` on fire with a molotov")
                         players[arr[i]][3]++
@@ -341,7 +350,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.95)
+                    else if(k<0.9)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` poisons `"+players[arr[i+1]][0]+"'s` food, killing him")
                         players[arr[i]][3]++
@@ -448,15 +457,24 @@ client.on('message', message => {
                 else if(j==2)
                 {
                     var k = Math.random()
-                    if(k<0.3 && f==0)
+                    if(k<0.2 && f==0)
                     message.channel.send("`"+players[arr[i]][0]+"` and `"+players[arr[i+1]][0]+"`"+d_lines[Math.floor(Math.random()*3)])
-                    else if(k<0.4)
-                    message.channel.send("`"+players[arr[i]][0]+"` attacks `"+players[arr[i+1]][0]+"`, but `"+players[arr[i+1]][0]+"` manages to escape")
-                    else if(k<0.5 && f==0)
+                    else if(k<0.3)
+                    message.channel.send("`"+players[arr[i]][0]+"` ambushes `"+players[arr[i+1]][0]+"`, but `"+players[arr[i+1]][0]+"` manages to escape")
+                    else if(k<0.4 && f==0)
                     message.channel.send("`"+players[arr[i]][0]+"` defeats `"+players[arr[i+1]][0]+"` in a fight, but spares their life.")
+                    else if(k<0.5)
+                    message.channel.send("`"+players[arr[i]][0]+"` secretly stalks `"+players[arr[i+1]][0]+"`")
+                    else if(k<0.55)
+                    {
+                        message.channel.send("`"+players[arr[i]][0]+"` smashes `"+players[arr[i+1]][0]+"'s` head with a bottle of vodka, *cyka blyat* style")
+                        players[arr[i]][3]++
+                        players[arr[i+1]][1]="Dead"
+                        arr.splice(i+1,1)
+                        i--
+                        console.log(arr)
+                    }
                     else if(k<0.6)
-                    message.channel.send("`"+players[arr[i]][0]+"` stalks `"+players[arr[i+1]][0]+"`")
-                    else if(k<0.65)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` stabs `"+players[arr[i+1]][0]+"` while his back is turned")
                         players[arr[i]][3]++
@@ -465,7 +483,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.7)
+                    else if(k<0.65)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` kills `"+players[arr[i+1]][0]+"` while he is resting")
                         players[arr[i]][3]++
@@ -474,7 +492,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.75)
+                    else if(k<0.7)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` strangles `"+players[arr[i+1]][0]+"` with a rope")
                         players[arr[i]][3]++
@@ -483,7 +501,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.8)
+                    else if(k<0.75)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` catches `"+players[arr[i+1]][0]+"` off guard and kills him")
                         players[arr[i]][3]++
@@ -492,7 +510,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.85)
+                    else if(k<0.8)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` tracks down and kills `"+players[arr[i+1]][0]+"`")
                         players[arr[i]][3]++
@@ -501,7 +519,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.9)
+                    else if(k<0.85)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` sets `"+players[arr[i+1]][0]+"` on fire with a molotov")
                         players[arr[i]][3]++
@@ -510,7 +528,7 @@ client.on('message', message => {
                         i--
                         console.log(arr)
                     }
-                    else if(k<0.95)
+                    else if(k<0.9)
                     {
                         message.channel.send("`"+players[arr[i]][0]+"` poisons `"+players[arr[i+1]][0]+"'s` food, killing him")
                         players[arr[i]][3]++
